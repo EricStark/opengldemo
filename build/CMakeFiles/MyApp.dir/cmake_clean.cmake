c723abe823167a039cce987e063bbf1a7e0be7c6
JOMA <1756564579@qq.com>
2024-04-16 11:15:33 +0800

    opengl 01 project structure

---
+++ build/CMakeFiles/MyApp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyApp.dir/Users/joma/cpp_project/opengl_test/src/test.cpp.o"
+  "CMakeFiles/MyApp.dir/Users/joma/cpp_project/opengl_test/src/test.cpp.o.d"
+  "MyApp"
+  "MyApp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyApp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/cocoa_init.m" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_init.m.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_init.m.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/cocoa_joystick.m" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_joystick.m.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/cocoa_monitor.m" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_monitor.m.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/cocoa_time.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_time.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_time.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/cocoa_window.m" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_window.m.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/cocoa_window.m.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/context.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/context.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/context.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/egl_context.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/egl_context.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/egl_context.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/init.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/init.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/init.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/input.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/input.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/input.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/monitor.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/monitor.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/monitor.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/nsgl_context.m" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/nsgl_context.m.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/nsgl_context.m.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/null_init.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/null_init.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/null_init.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/null_joystick.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/null_joystick.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/null_joystick.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/null_monitor.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/null_monitor.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/null_monitor.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/null_window.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/null_window.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/null_window.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/osmesa_context.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/osmesa_context.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/osmesa_context.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/platform.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/platform.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/platform.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/posix_module.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/posix_module.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/posix_module.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/posix_thread.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/posix_thread.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/posix_thread.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/vulkan.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/vulkan.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/vulkan.c.o.d"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/src/window.c" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/window.c.o" "gcc" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/CMakeFiles/glfw.dir/window.c.o.d"
+  )
+
+# Pairs of files generated by the same build rule.
+set(CMAKE_MULTIPLE_OUTPUT_PAIRS
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/libglfw.3.dylib" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/libglfw.3.4.dylib"
+  "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/libglfw.dylib" "/Users/joma/cpp_project/opengl_test/third_dependencies/GLFW/glfw-3.4/build/src/libglfw.3.4.dylib"
+  )
+
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
